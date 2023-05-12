2e3eb1566632a92d4cebbcb18cf4969b5302ebc6
HaniehAbdolahi <122110903+HaniehAbdolahi@users.noreply.github.com>
2023-05-12 08:21:01 +0430

    added source code github url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
       <Search />
+      <div>
+        <a href=" github.com/HaniehAbdolahi/weather_react_app1.">
+          source-coded
+        </a>{" "}
+        by Hanieh Abdolahi
+      </div>
     </div>
   );
 }
