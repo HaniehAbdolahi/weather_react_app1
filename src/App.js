d8704b729c40b346c2d33f00b780bcfc7a462a0b
HaniehAbdolahi <122110903+HaniehAbdolahi@users.noreply.github.com>
2023-05-12 12:48:58 +0430

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Search from "./Search";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <Search />
-      <div>
+      <footer>
         <a href="https://github.com/HaniehAbdolahi/weather_react_app1">
-          source-coded
+          source-coded on Github
         </a>{" "}
         by Hanieh Abdolahi
-      </div>
+      </footer>
     </div>
   );
 }
